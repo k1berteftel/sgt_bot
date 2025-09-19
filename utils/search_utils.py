b7ad6e6fba138950eac67d234c376105d250b9ae
k1berteftel <kkulis985@gmail.com>
2025-09-20 01:28:00 +0300

    fix

--- utils/search_utils.py
+++ utils/search_utils.py
@@ -4,9 +4,11 @@ import re
 def parse_entities(text):
     worker_match = re.search(r'Воркер:\s*@(\w+)', text)
     username = worker_match.group(1) if worker_match else None
-    match = re.match(r'^(\d+(?:[.,]\d+)?)\s*([^\d\s]+)?$', text.strip())
-    if match:
-        amount = int(match.group(1).replace(',', '.'))
-        currency_symbol = match.group(2) or 'USD'  # если нет символа — считаем USD
-        return username, amount, currency_symbol
+    profit_match = re.search(r'Доля воркера:\s*(\d+(?:[.,]\d+)?)\s*([€$£₽]|[A-Z]{3})?', text)
+    if profit_match:
+        amount_str = profit_match.group(1).replace(',', '.')
+        amount = int(float(amount_str))  # или float(amount_str), если нужны дробные
+        currency = profit_match.group(2) or 'USD'  # если валюта не указана — USD по умолчанию
+        return username, amount, currency
+
     return None, None, None
